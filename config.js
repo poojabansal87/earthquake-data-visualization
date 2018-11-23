7e0ad485d002be41cc7e8ee3c11e19752e9b6893
Pooja <poojabansal87@gmail.com>
2018-11-22 18:22:50 -0800

    Initial Draft of the geoJson with map and circleMarkers

---
+++ config.js
@@ -0,0 +1,3 @@
+// API key
+const API_KEY = "pk.eyJ1Ijoia3VsaW5pIiwiYSI6ImNpeWN6bjJ0NjAwcGYzMnJzOWdoNXNqbnEifQ.jEzGgLAwQnZCv9rA6UTfxQ";
+// const API_KEY = "pk.eyJ1IjoiamJjaGVuZXR0ZSIsImEiOiJjam85bnpvYnkwNndxM3BuenZmdmFhM2wwIn0.8BlVXRD0syuxl_HBzsVMJA";
